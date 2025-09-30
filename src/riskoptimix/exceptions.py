f38d71ca9cbba157eb020dd6ec79bedf050a7fd3
ThomasvdHulst03 <t.van.der.hulst@student.vu.nl>
2025-09-30 09:57:36 +0200

    Added data module

---
+++ src/riskoptimix/exceptions.py
@@ -0,0 +1,21 @@
+"""Custom exceptions for RiskOptimix"""
+
+
+class RiskOptimixError(Exception):
+    """Base exception for all RiskOptimix errors"""
+    pass
+
+
+class DataError(RiskOptimixError):
+    """Raised when data cannot be fetched or is invalid"""
+    pass
+
+
+class ValidationError(RiskOptimixError):
+    """Raised when input validation fails"""
+    pass
+
+
+class BacktestError(RiskOptimixError):
+    """Raised when backtest encounters an error"""
+    pass
